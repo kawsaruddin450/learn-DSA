c5ba31f584bcd5177b24e310bf52c688a59dd97f
kawsar-uddin <kawsaruddin450@gmail.com>
2025-01-24 15:57:57 +0600

    singly linked list create node and delete node

---
+++ singly_linked_list.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node Node;
+struct node{
+    int data;
+    Node *next;
+};
+
+Node *create_node(int item, Node *next){
+    Node *new_node = (Node *) malloc(sizeof(Node));
+    if(new_node==NULL){
+        printf("Error! could not create a new node.\n");
+        exit(1);
+    }
+
+    new_node->data = item;
+    new_node->next = next;
+
+    return new_node;
+}
+
+Node *remove_node(Node *head, Node *node){
+    if(node == head){
+        head = node->next;
+        free(node);
+        return head;
+    }else{
+        Node *current_node = head;
+        while(current_node != NULL){
+            if(current_node->next == node){
+                break;
+            }
+            current_node = current_node->next;
+        }
+
+        if(current_node==NULL){
+            return head;
+        }else{
+            current_node->next = node->next;
+            free(node);
+            return head;
+        }
+    }
+}
+
+int main(){
+    Node *n;
+    n = create_node(10, NULL);
+    printf("%d\n", n->data);
+
+    return 0;
+}
