999777fb8f9b178f4aeec8176e2f11b6cdb60f63
kawsar-uddin <kawsaruddin450@gmail.com>
2025-01-29 00:48:29 +0600

    Max and Min finding function

--- DS-in-cpp/array_adt.cpp
+++ DS-in-cpp/array_adt.cpp
@@ -111,6 +111,28 @@ int BinarySearch(Array arr, int key){
     return -1;
 }
 
+//find the max function
+int Max(Array arr){
+    int max = arr.A[0];
+    for(int i=1; i<arr.length; i++){
+        if(arr.A[i]>max){
+            max = arr.A[i];
+        }
+    }
+    return max;
+}
+
+//find the minimum function
+int Min(Array arr){
+    int min = arr.A[0];
+    for(int i=1; i<arr.length; i++){
+        if(arr.A[i]<min){
+            min = arr.A[i];
+        }
+    }
+    return min;
+}
+
 int main()
 {
     struct Array arr = {{10, 11, 15, 18, 21, 24, 28, 31, 35, 39, 40, 56, 59, 80}, 20, 14};
@@ -118,6 +140,8 @@ int main()
 
     Set(&arr, 6, 6);
     cout << Get(arr, 7) << endl;
+    cout << Max(arr) << endl;
+    cout << Min(arr) << endl;
     Display(arr);
 
     return 0;
