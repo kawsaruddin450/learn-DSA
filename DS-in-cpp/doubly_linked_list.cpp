22190623a047a31d15343b9d7d1a40a67dfb23ab
kawsar-uddin <kawsaruddin450@gmail.com>
2025-01-31 19:53:17 +0600

    Doubly linked list: create and display

---
+++ DS-in-cpp/doubly_linked_list.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    Node *prev;
+    int data;
+    Node *next;
+};
+class DoublyList
+{
+private:
+    Node *first;
+
+public:
+    DoublyList()
+    {
+        first = NULL;
+    }
+    DoublyList(int A[], int n);
+    void Display();
+};
+
+/*********************************************************************
+                        MAIN FUNCTION
+**********************************************************************/
+
+int main()
+{
+    int A[] = {1, 2, 3, 4, 5};
+    DoublyList dl(A, 5);
+    dl.Display();
+
+    return 0;
+}
+
+/*********************************************************************
+                        END
+**********************************************************************/
+
+DoublyList::DoublyList(int A[], int n)
+{
+    Node *t, *p, *last;
+    p = new Node;
+    p->data = A[0];
+    p->next = NULL;
+    p->prev = NULL;
+    first = p;
+    last = p;
+    for (int i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->next = NULL;
+        t->prev = last;
+        last->next = t;
+        last = t;
+    }
+}
+
+void DoublyList::Display()
+{
+    Node *p = first;
+    while (p)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
