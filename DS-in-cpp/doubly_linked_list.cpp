80205c2ccc95de5b5e53b956ff959e843eca4e63
kawsar-uddin <kawsaruddin450@gmail.com>
2025-01-31 20:57:21 +0600

    Delete an element and display reversely a doubly linked list

--- DS-in-cpp/doubly_linked_list.cpp
+++ DS-in-cpp/doubly_linked_list.cpp
@@ -20,8 +20,10 @@ public:
     }
     DoublyList(int A[], int n);
     void Display();
+    void DisplayReverse();
     int Length();
     void Insert(int pos, int x);
+    void Delete(int pos);
 };
 
 /*********************************************************************
@@ -32,8 +34,9 @@ int main()
 {
     int A[] = {1, 2, 3, 4, 5};
     DoublyList dl(A, 5);
-    dl.Insert(5, 10);
+    dl.Delete(4);
     dl.Display();
+    dl.DisplayReverse();
     cout << "Length of the list: " << dl.Length() << endl;
 
     return 0;
@@ -72,6 +75,21 @@ void DoublyList::Display()
         p = p->next;
     }
     cout << endl;
+}
+
+void DoublyList::DisplayReverse()
+{
+    Node *p = first;
+    while (p->next)
+    {
+        p = p->next;
+    }
+    while(p){
+        cout << p->data << " ";
+        p=p->prev;
+    }
+    cout << endl;
+    
 }
 
 int DoublyList::Length()
@@ -118,6 +136,38 @@ void DoublyList::Insert(int pos, int x)
             q = t->next;
             if (q)
                 q->prev = t;
+        }
+    }
+}
+void DoublyList::Delete(int pos)
+{
+    if (pos < 0 || pos > this->Length())
+    {
+        return;
+    }
+    else
+    {
+        if (pos == 1)
+        {
+            Node *p = first;
+            first = first->next;
+            if (first)
+                first->prev = NULL;
+            delete p;
+        }
+        else
+        {
+            Node *p = first;
+            for (int i = 1; i < pos; i++)
+            {
+                p = p->next;
+            }
+            (p->prev)->next = p->next;
+            if (p->next)
+            {
+                (p->next)->prev = p->prev;
+            }
+            delete p;
         }
     }
 }
