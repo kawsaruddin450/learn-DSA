87ed3e5374eb3bbfb8aa1b2a46229c365de106e0
kawsar-uddin <kawsaruddin450@gmail.com>
2025-02-01 19:49:44 +0600

    Doubly circular linked list refurbished

--- DS-in-cpp/doubly_circular_linked_list.cpp
+++ DS-in-cpp/doubly_circular_linked_list.cpp
@@ -43,9 +43,9 @@ DoublyCircular::DoublyCircular(int A[], int n)
     Node *last, *t, *p;
     p = new Node;
     p->data = A[0];
-    head = p;
     p->prev = p;
     p->next = p;
+    head = p;
     last = p;
     for (int i = 1; i < n; i++)
     {
@@ -68,16 +68,93 @@ void DoublyCircular::Display()
     cout << endl;
 }
 
-int DoublyCircular::length(){
+int DoublyCircular::length()
+{
     Node *p = head;
-    int count=0;
-    if(head==NULL){
+    int count = 0;
+    if (head == NULL)
+    {
         return 0;
     }
-    do{
+    do
+    {
         count++;
-        p=p->next;
-    }while(p!=head);
+        p = p->next;
+    } while (p != head);
 
     return count;
 }
+
+// void DoublyCircular::Insert(int pos, int x)
+// {
+//     if (pos < 0 || pos > this->length())
+//     {
+//         cout << "Index out of range. Insertion not possible." << endl;
+//         return;
+//     }
+//     else
+//     {
+//         Node *t = new Node;
+//         t->data = x;
+//         if (pos == 0)
+//         {
+//             Node *last = head->prev;
+//             t->next = head;
+//             t->prev = last;
+//             last->next = t;
+//             head->prev = t;
+//             head = t;
+//         }
+//         else
+//         {
+//             Node *p = head;
+//             for (int i = 1; i < pos; i++)
+//             {
+//                 p = p->next;
+//             }
+//             t->next = p->next;
+//             t->prev = p;
+//             (p->next)->prev = t;
+//             p->next = t;
+//         }
+//     }
+// }
+
+// void DoublyCircular::Delete(int pos)
+// {
+//     if (pos < 0 || pos > this->length())
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         Node *p = head;
+//         if (pos == 0)
+//         {
+//             if (head->next == head)
+//             {
+//                 head = NULL;
+//             }
+//             else
+//             {
+//                 Node *tail = head->prev;
+//                 head = head->next;
+//                 head->prev = tail;
+//                 tail->next = head;
+//             }
+//             delete p;
+//         }
+//         else
+//         {
+//             for (int i = 1; i < pos; i++)
+//             {
+//                 p = p->next;
+//             }
+//             (p->prev)->next = p->next;
+//             (p->next)->prev = p->prev;
+//             delete p;
+//         }
+//     }
+// }
+
+
